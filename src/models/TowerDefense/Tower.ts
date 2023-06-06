96f15a43019c6284468466f7c1e3a1732c968440
Vic1228 <95247372+Vic1228@users.noreply.github.com>
2023-06-06 18:07:44 +0800

    塔房++

--- src/models/TowerDefense/Tower.ts
+++ src/models/TowerDefense/Tower.ts
@@ -1,38 +1,101 @@
-// 抽象防禦塔
-abstract class DefenseTower {
-  abstract level: number
-  abstract attack(): void
-  abstract upgrade(): void
+// 定義防禦塔介面
+interface Tower {
+  level: number
+  startAttackTimer(): void
+  stopAttackTimer(): void
+  upgrade(): void
 }
 
-// 具體的防禦塔類
-export class ConcreteDefenseTower extends DefenseTower {
+// 具體的防禦塔類別
+class ConcreteTower implements Tower {
   level: number
+  private attackTimer: any | null = null
 
   constructor(level: number) {
-    super()
     this.level = level
   }
 
-  attack(): void {
-    console.log(`防禦塔攻擊，等級：${this.level}`)
+  upgrade(): void {
+    this.level++
   }
 
-  upgrade(): void {
-    console.log('防禦塔升級')
-    this.level++
+  startAttackTimer(): void {
+    this.attackTimer = setInterval(() => {}, this.getAttackSpeed())
+  }
+
+  stopAttackTimer(): void {
+    if (this.attackTimer) {
+      clearInterval(this.attackTimer)
+      this.attackTimer = null
+    }
+  }
+
+  private getAttackSpeed(): number {
+    return 1000
   }
 }
 
-// 防禦塔享元工廠
-class DefenseTowerFlyweightFactory {
-  private towers: { [key: string]: DefenseTower } = {}
+// 防禦塔裝飾者介面
+interface TowerDecorator extends Tower {}
 
-  createTower(level: number): DefenseTower {
-    const key = `${level}`
-    if (!this.towers[key]) {
-      this.towers[key] = new ConcreteDefenseTower(level)
-    }
-    return this.towers[key]
+// 增加攻擊力的防禦塔裝飾
+class AttackTowerDecorator implements TowerDecorator {
+  private tower: Tower
+
+  constructor(tower: Tower) {
+    this.tower = tower
+  }
+
+  get level(): number {
+    return this.tower.level
+  }
+
+  startAttackTimer(): void {}
+  stopAttackTimer(): void {}
+
+  upgrade(): void {
+    this.tower.upgrade()
+    console.log('攻擊力增加')
+  }
+}
+
+// 防禦塔工廠
+class TowerFactory {
+  static createTower(level: number): Tower {
+    return new ConcreteTower(level)
+  }
+}
+
+// 防禦塔生成器管理器
+export class TowerGenerator {
+  private towerList: { tower: Tower | null; location: number }[] = Array.from(
+    { length: 6 },
+    () => ({ tower: null, location: 0 })
+  )
+
+  // 建造防禦塔
+  // 在 TowerGenerator 中生成防禦塔時，傳遞攻擊速度
+  generateTower(level: number, location: number, attackSpeed: number): Tower {
+    const tower = TowerFactory.createTower(level)
+    tower.attackSpeed = 300 // 設定攻擊速度
+    tower.attackTimer = null // 初始化攻擊計時器為空
+    this.towerList[location].tower = tower
+    this.towerList[location].location = location
+    return tower
+  }
+
+  // 升級防禦塔
+  upgradeTower(tower: Tower): void {
+    tower.upgrade()
+  }
+
+  // 為防禦塔添加攻擊力裝飾
+  addAttackDecorator(tower: Tower): Tower {
+    return new AttackTowerDecorator(tower)
+  }
+
+  // 獲取當前防禦塔列表
+  getTowerList(): { tower: Tower | null; location: number }[] {
+    return this.towerList.slice()
   }
 }
--- src/views/TowerDefense/TowerDefense.vue
+++ src/views/TowerDefense/TowerDefense.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="layout">
     <div class="td-container">
-      <div style="color:black">初始血量：{{ initialBloodVolume }}</div>
+      <div style="color:black">血量：{{ initialBloodVolume }}</div>
       <div class="monster-way">
         <div v-for="(ele, idx) in monsterList" :key="idx" class="monster-body">
           <img :style="'left:' + ele.location + 'px'" src="@/assets/photo/monster.png" alt="">
+        </div>
+      </div>
+      <div class="tower-space">
+        <div v-for="(ele, idx) in towerList" :key="idx" class="tower-body">
+          <img v-if="ele.tower" src="@/assets/photo/cat.png" alt="">
+          <div v-else @click="createTower(idx)" class="tower-noDisplay"></div>
         </div>
       </div>
     </div>
@@ -12,46 +18,62 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { MonsterGenerator, Monster } from '@/models/TowerDefense/Monster';
+import { ref } from 'vue';
+import { MonsterGenerator, Monster, MonsterObserver } from '@/models/TowerDefense/Monster';
+import { TowerGenerator } from '@/models/TowerDefense/Tower';
 
 // -- ref -- //
-const monsterList: any = ref([]) // 怪物清單
-const initialBloodVolume = ref(100) // 初始血量
+const monsterList = ref<{ monster: Monster; location: number }[]>([]); // 怪物清單
+const towerList = ref<any>([]); // 防禦塔清單
+const initialBloodVolume = ref(100); // 初始血量
 
 // -- created -- //
-let monsterGenerationInterval: any;
-let monsterMovementInterval: any;
-const monsterGenerator = new MonsterGenerator()
-const monsterObserver = {
-  onMonsterMoved: () => {
-    monsterList.value = monsterGenerator.getMonsterList()
+//* 創建怪物觀察者
+const monsterObserver: MonsterObserver = {
+  // 怪物移動
+  onMonsterMoved() {
+    monsterList.value = monsterGenerator.getMonsterList();
   },
-  onMonsterCreated: (monster: Monster) => {
-    console.log("新怪物")
+  // 怪物創造
+  onMonsterCreated() {
+    monsterList.value = monsterGenerator.getMonsterList();
   },
-  onMonsterExceededThreshold: () => {
-    initialBloodVolume.value--
+  // 怪物超出範圍
+  onMonsterExceededThreshold() {
+    initialBloodVolume.value--;
     if (initialBloodVolume.value <= 0) {
       clearInterval(monsterGenerationInterval);
       clearInterval(monsterMovementInterval);
     }
-  }
+  },
+};
+
+
+//* 創建怪物生成器
+const monsterGenerator = new MonsterGenerator();
+monsterGenerator.addObserver(monsterObserver);
+
+//* 創建防禦塔生成器
+const towerGenerator = new TowerGenerator()
+towerList.value = towerGenerator.getTowerList()
+
+//* 設定怪物生成和移動的間隔時間
+const monsterGenerationInterval = setInterval(() => {
+  monsterGenerator.generateMonster(1, 70, 1, 70);
+}, 500);
+
+const monsterMovementInterval = setInterval(() => {
+  monsterGenerator.moveMonster(towerList.value);
+}, 10);
+
+// -- method -- //
+function createTower(index: number): void {
+  towerGenerator.generateTower(1, index)
 }
-monsterGenerator.addObserver(monsterObserver)
-
-monsterGenerationInterval = setInterval(() => {
-  monsterGenerator.generateMonster(1, 100, 1)
-}, 500)
-
-monsterMovementInterval = setInterval(() => {
-  monsterGenerator.moveMonster()
-}, 10)
-
-
-
 
 </script>
+
+
 
 <style lang="scss">
 .td-container {
@@ -65,17 +87,36 @@ monsterMovementInterval = setInterval(() => {
     margin-top: 10%;
     border-bottom: solid 1px black;
     border-top: solid 1px black;
-    height: 10%
-  }
+    height: 10%;
 
-  .monster-body {
-    position: relative;
-    height: 100%;
-
-    img {
-      position: absolute;
+    .monster-body {
+      position: relative;
       height: 100%;
+
+      img {
+        position: absolute;
+        height: 100%;
+      }
     }
   }
+
+  .tower-space {
+    display: flex;
+
+    .tower-body {
+      width: calc(100%/6);
+
+      .tower-noDisplay {
+        border: 1px solid black;
+        height: 100%;
+        min-height: 100px;
+      }
+
+      img {
+        width: 100%
+      }
+    }
+  }
+
 }
 </style>
